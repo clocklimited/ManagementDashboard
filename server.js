9cd0e9e297b5c2597d9f0426233d47ddc3003c8f
Jack Burgess <jackburges124@gmail.com>
2016-09-08 15:47:18 +0100

    Improve server logging

--- server.js
+++ server.js
@@ -7,28 +7,34 @@ var express = require('express')
 
 app.set('view engine', 'pug')
 app.use(express.static(__dirname))
+app.enable('trust proxy')
 
 app.get('/', function (req, res) {
+  console.log('GET: / from IP', req.ip)
   res.render('index')
 })
 
 app.get('/data', function (req, res) {
+  console.log('GET: /data from IP', req.ip)
   SheetsHelper.service.spreadsheets.values.get({
     spreadsheetId
     , range: 'Data'
   }, function sheetReady (err, sheet) {
     if (err) console.log(err)
     res.status(200).json(sheet)
+    console.log('GET: /data from IP %s sent `data` sheet', req.ip)
   })
 })
 
 app.get('/targets', function (req, res) {
+  console.log('GET: /targets from IP', req.ip)
   SheetsHelper.service.spreadsheets.values.get({
     spreadsheetId
     , range: 'Targets'
   }, function sheetReady (err, sheet) {
     if (err) console.log(err)
     res.status(200).json(sheet)
+    console.log('GET: /targets from IP %s sent `targets` sheet', req.ip)
   })
 })
 
