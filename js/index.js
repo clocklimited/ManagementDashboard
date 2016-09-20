6f76d18dd84dae55c6704847688b86bbe96e53d3
Jack Burgess <jackburges124@gmail.com>
2016-09-20 11:29:33 +0100

    Changing the date no longer needs a page refresh or another ajax req

--- js/index.js
+++ js/index.js
@@ -9,80 +9,86 @@ const createPieChart = require('./create-pie-chart-google')
     , addToggles = require('./add-toggles')
 
 let spreadSheetData = [ ]
-  , data = {
-      tickets: { }
-    , costs: { }
-    , target: { }
-    , staffSatisfaction: { }
-    , clientSatisfaction: { }
-  }
+  , data
   , spreadSheetTargets = [ ]
   , formatter
 
 function getSpreadsheetData () {
   return $.ajax({
     url: '/data'
-  , success: (body) => {
-      console.log('Data', body.values)
-      spreadSheetData = body.values
-      let currentMonth = getUrlParameter('month') || moment().format('MMMM YYYY')
-        , currentMonthIndex = spreadSheetData[pos.BOARDPACK_DATE].indexOf(currentMonth) + 1
-        , range = 6
-        // Prevents going out of bounds
-        , start = currentMonthIndex <= range ? 1 : currentMonthIndex - range
-        , end = currentMonthIndex
-
-      data.dates = spreadSheetData[pos.MONTH].slice(start, end)
-      data.dates = data.dates.map((date) => moment(date, 'MMMM YYYY').format('MMM YY'))
-      formatter = createFormatter(data.dates)
-      // FINANCE
-      data.revenue = formatter.format({ Revenue: spreadSheetData[pos.REVENUE].slice(start, end) })
-      data.profit = formatter.format({ Profit: spreadSheetData[pos.PROFIT].slice(start, end) })
-      data.costs.Staff = spreadSheetData[pos.COSTS_STAFF].slice(start, end)
-      data.costs.Total = spreadSheetData[pos.COSTS_TOTAL].slice(start, end)
-      data.costs = formatter.format(data.costs)
-      data.annuity = formatter.format({ Annuity: spreadSheetData[pos.ANNUITY].slice(start, end) })
-      data.revenuePerHead = formatter.format({ RPH: spreadSheetData[pos.REVENUE_PER_HEAD].slice(start, end) })
-      // SALES
-      data.closedDeals = formatter.format({ ClosedDeals: spreadSheetData[pos.CLOSED_DEALS].slice(start, end) })
-      data.pipeline = formatter.format({ Pipeline: spreadSheetData[pos.PIPELINE].slice(start, end) })
-      // PRODUCTION
-      data.tickets.Opened = spreadSheetData[pos.TICKETS_OPENED].slice(start, end)
-      data.tickets.Closed = spreadSheetData[pos.TICKETS_CLOSED].slice(start, end)
-      data.tickets = formatter.format(data.tickets)
-      // HR
-      data.headCount = formatter.format({ HeadCount: spreadSheetData[pos.HEAD_COUNT].slice(start, end) })
-      data.sickDays = formatter.format({ SickDays: spreadSheetData[pos.SICK_DAYS].slice(start, end) })
-      data.staffSatisfaction.value = spreadSheetData[pos.STAFF_SATISFACTION][end - 1]
-      data.clientSatisfaction.value = spreadSheetData[pos.CLIENT_SATISFACTION][end - 1]
-
-      console.log('Formatted Data', data)
-      getSpreadsheetTargets()
-    }
+  , success: (body) => processSpreadsheetData(body)
   })
 }
 
 function getSpreadsheetTargets () {
   return $.ajax({
     url: '/targets'
-  , success: (body) => {
-      console.log('Target', body.values)
-      spreadSheetTargets = body.values
-      data.target.revenue = spreadSheetTargets[pos.TARGETS.REVENUE][1]
-      data.target.profit = spreadSheetTargets[pos.TARGETS.PROFIT][1]
-      data.target.revenuePerHead = spreadSheetTargets[pos.TARGETS.REVENUE_PER_HEAD][1]
-      data.staffSatisfaction.target = spreadSheetTargets[pos.TARGETS.STAFF_SATISFACTION][1]
-      data.clientSatisfaction.target = spreadSheetTargets[pos.TARGETS.CLIENT_SATISFACTION][1]
-      data.revenueVsTarget = formatter.formatItemVsTarget({ Target: data.target.revenue, Revenue: data.revenue })
-      data.profitVsTarget = formatter.formatItemVsTarget({ Target: data.target.profit, Profit: data.profit })
-      data.revenuePerHeadVsTarget = formatter.formatItemVsTarget({
-        Target: data.target.revenuePerHead
-      , RPH: data.revenuePerHead
-      }, true)
-      data.revenueVsTargetPie = formatter.formatPieChart(data.target.revenue, data.revenue)
-      repopulate()
-    }
+  , success: (body) => processSpreadsheetTargets(body)
   })
+}
+
+function processSpreadsheetData (body, month) {
+  spreadSheetData = spreadSheetData.length > 1 ? spreadSheetData : body.values
+  let currentMonth = month || moment().format('MMMM YYYY')
+    , currentMonthIndex = spreadSheetData[pos.BOARDPACK_DATE].indexOf(currentMonth) + 1
+    , range = 6
+    // Prevents going out of bounds
+    , start = currentMonthIndex <= range ? 1 : currentMonthIndex - range
+    , end = currentMonthIndex
+
+  console.log(currentMonth, currentMonthIndex, start, end)
+  // Reset data
+  data = {
+      tickets: { }
+    , costs: { }
+    , target: { }
+    , staffSatisfaction: { }
+    , clientSatisfaction: { }
+  }
+
+  data.dates = spreadSheetData[pos.MONTH].slice(start, end)
+  data.dates = data.dates.map((date) => moment(date, 'MMMM YYYY').format('MMM YY'))
+  formatter = createFormatter(data.dates)
+  // FINANCE
+  data.revenue = formatter.format({ Revenue: spreadSheetData[pos.REVENUE].slice(start, end) })
+  data.profit = formatter.format({ Profit: spreadSheetData[pos.PROFIT].slice(start, end) })
+  data.costs.Staff = spreadSheetData[pos.COSTS_STAFF].slice(start, end)
+  data.costs.Total = spreadSheetData[pos.COSTS_TOTAL].slice(start, end)
+  data.costs = formatter.format(data.costs)
+  data.annuity = formatter.format({ Annuity: spreadSheetData[pos.ANNUITY].slice(start, end) })
+  data.revenuePerHead = formatter.format({ RPH: spreadSheetData[pos.REVENUE_PER_HEAD].slice(start, end) })
+  // SALES
+  data.closedDeals = formatter.format({ ClosedDeals: spreadSheetData[pos.CLOSED_DEALS].slice(start, end) })
+  data.pipeline = formatter.format({ Pipeline: spreadSheetData[pos.PIPELINE].slice(start, end) })
+  // PRODUCTION
+  data.tickets.Opened = spreadSheetData[pos.TICKETS_OPENED].slice(start, end)
+  data.tickets.Closed = spreadSheetData[pos.TICKETS_CLOSED].slice(start, end)
+  data.tickets = formatter.format(data.tickets)
+  // HR
+  data.headCount = formatter.format({ HeadCount: spreadSheetData[pos.HEAD_COUNT].slice(start, end) })
+  data.sickDays = formatter.format({ SickDays: spreadSheetData[pos.SICK_DAYS].slice(start, end) })
+  data.staffSatisfaction.value = spreadSheetData[pos.STAFF_SATISFACTION][end - 1]
+  data.clientSatisfaction.value = spreadSheetData[pos.CLIENT_SATISFACTION][end - 1]
+
+  if (!month) getSpreadsheetTargets()
+  else processSpreadsheetTargets()
+}
+
+function processSpreadsheetTargets (body) {
+  spreadSheetTargets = spreadSheetTargets.length > 1 ? spreadSheetTargets : body.values
+  data.target.revenue = spreadSheetTargets[pos.TARGETS.REVENUE][1]
+  data.target.profit = spreadSheetTargets[pos.TARGETS.PROFIT][1]
+  data.target.revenuePerHead = spreadSheetTargets[pos.TARGETS.REVENUE_PER_HEAD][1]
+  data.staffSatisfaction.target = spreadSheetTargets[pos.TARGETS.STAFF_SATISFACTION][1]
+  data.clientSatisfaction.target = spreadSheetTargets[pos.TARGETS.CLIENT_SATISFACTION][1]
+  data.revenueVsTarget = formatter.formatItemVsTarget({ Target: data.target.revenue, Revenue: data.revenue })
+  data.profitVsTarget = formatter.formatItemVsTarget({ Target: data.target.profit, Profit: data.profit })
+  data.revenuePerHeadVsTarget = formatter.formatItemVsTarget({
+    Target: data.target.revenuePerHead
+  , RPH: data.revenuePerHead
+  }, true)
+  data.revenueVsTargetPie = formatter.formatPieChart(data.target.revenue, data.revenue)
+  repopulate()
 }
 
 function repopulate () {
@@ -118,11 +124,6 @@ function repopulate () {
   addDetails('#client-satisfaction', width, height / 2, data.clientSatisfaction)
 }
 
-function getUrlParameter (name) {
-  return decodeURIComponent((new RegExp('[?|&]' + name + '=' + '([^&;]+?)(&|#|;|$)').exec(window.location.search)
-    || [null, ''])[1].replace(/\+/g, '%20')) || null
-}
-
 google.charts.load('current', { packages: [ 'corechart' ] })
 google.charts.setOnLoadCallback(getSpreadsheetData)
 
@@ -133,6 +134,11 @@ $('.js-month-picker').MonthPicker({
   Button: '<button class=\'flat-button\'>Select Date</button>'
 , MonthFormat: 'MM yy'
 , OnAfterMenuClose: function () {
-  window.location = window.location.origin + '/show?month=' + $('.js-month-picker').val()
+  window.location.hash = $('.js-month-picker').val()
 }
+})
+
+$(window).bind('hashchange', function () {
+  var month = decodeURIComponent(window.location.hash).replace('#', '')
+  processSpreadsheetData(null, month)
 })
